eaedd9a2a08d1f4010e24fb5c4d3254b32705c0b
boxfisch <ptr5044@gmail.com>
2023-09-20 21:46:03 -0400

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Svelte Planets
+An exploration though the cosmos 🪐
+
+## Ideas
+I love using svelte transitions for interactions on data visualizations -- but for more complex movements I prefer to use `d3-select`. Here is a quick attempt at mixing the two in a charming planet visual.
---
+++ src/assets/data.js
@@ -0,0 +1,27 @@
+export default [
+  {
+    name: "Jupiter",
+    size: 600,
+  },
+  {
+    name: "Earth",
+    size: 200,
+  },
+  {
+    name: "Pluto",
+    size: 800,
+  },
+  {
+    name: "Neptune",
+    size: 700,
+  },
+  {
+    name: "Uranus",
+    size: 600,
+  },
+  {
+    name: "Moon",
+    size: 100,
+  },
+];
+
